3b6f17d79a228100dd300403b05c04229002eab9
compsci <ConnorsGithubv1>
2019-10-28 10:00:19 -0600

    bpa practice

--- 2019BPAReg/src/MovieRate.java
+++ 2019BPAReg/src/MovieRate.java
@@ -1,14 +1,38 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class MovieRate {
+	
+	private int movies = 0;
 
-	public MovieRate() {
+	public MovieRate(int i) {
 		// TODO Auto-generated constructor stub
-		
+		movies = i;
 	}
 
-	public void start(int i, int j , int k) {
+	public void start() {
 		// TODO Auto-generated method stub
-		
+		System.out.println(movies);
+		while (movies < 0 || movies > 3) {
+			System.out.println(movies);
+			System.out.println("error");
+			String input = JOptionPane.showInputDialog("enter movies 1 to 3:");
+			movies = Integer.parseInt(input);
+		}
+	}
+	
+	public void website() {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		for(int b = 1; b < movies + 1; b++) {
+			System.out.println("Please enter ratings for the movie review #"+ b);
+			double review1 = scanner.nextDouble(); 
+			double review2 = scanner.nextDouble(); 
+			double review3 = scanner.nextDouble(); 
+			System.out.println(review1);
+			System.out.println(review2);
+			System.out.println(review3);
+		}
 	}
 	
 	public void focusgroup(double c , double d) {
@@ -18,5 +42,7 @@ public class MovieRate {
 	public void average(double a) {
 		
 	}
+
+	
 
 }
--- 2019BPAReg/src/MovierateRunner.java
+++ 2019BPAReg/src/MovierateRunner.java
@@ -3,12 +3,11 @@ public class MovierateRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		temp data = new temp();
-		MovieRate program = new MovieRate();
+		MovieRate program = new MovieRate(4);
 		
-		data.setVisible(true);
 		
-		program.start(0, 0, 0);
+		program.start();
+		program.website();
 		program.focusgroup(2.0, 2.0);
 		program.average(2.0);
 	}
