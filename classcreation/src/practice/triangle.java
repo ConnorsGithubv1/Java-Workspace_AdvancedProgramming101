f10924753c550fd3695d404ea6989b9baaaaa311
connorlemon <connorsmith0403@gmail.com>
2019-11-06 22:46:04 -0800

    class labs finished

--- classcreation/src/practice/triangle.java
+++ classcreation/src/practice/triangle.java
@@ -1,9 +1,44 @@
 package practice;
 
+import java.util.Scanner;
+
+
 public class triangle {
+	
+	private double a;
+	private double b;
+	private double c;
+	private double s;
+	private double area;
 
 	public triangle() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public void getvalues() {
+		// TODO Auto-generated method stub
+		Scanner S = new Scanner(System.in);
+		System.out.println("enter side A : ");
+		a = S.nextInt();
+		System.out.println("enter side B : ");
+		b = S.nextInt();
+		System.out.println("enter side C : ");
+		c = S.nextInt();
+		System.out.println(" ");
+	}
+	
+	public double getperimeter() {
+		// TODO Auto-generated method stub
+		return a + b + c;
+	}
+
+	public double findarea() {
+		// TODO Auto-generated method stub
+		s = getperimeter() / 2;
+		System.out.println(a + " " + b + " " + c);
+		area = (double) Math.sqrt( s*(s -a)*(s - b)*(s - c));
+		return area;
+	}
+
 
 }
--- classcreation/src/practice/triangleRunner.java
+++ classcreation/src/practice/triangleRunner.java
@@ -5,6 +5,10 @@ public class triangleRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		triangle t = new triangle();
+		t.getvalues();
+		System.out.println(t.getperimeter());
+		System.out.println(" ");
+		System.out.println("Area == " + t.findarea());
 	}
 
 }
